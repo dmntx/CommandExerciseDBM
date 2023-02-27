759a0940bc0e047da552fb895aee84110b93a795
JUAN BERNAL MARTINEZ <juan.bernalm@alumno.buap.mx>
2023-02-26 23:59:27 -0600

    Agregar archivos de proyecto.

---
+++ CommandExerciseDBM/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommandExerciseDBM
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // The client code can parameterize an invoker with any commands.
+            Invoker invoker = new Invoker();
+            invoker.SetOnStart(new SimpleCommand("Hola"));
+            Receiver receiver = new Receiver();
+            invoker.SetOnFinish(new ComplexCommand(receiver, "Apaga la luz", "Enciende la luz"));
+
+            invoker.DoSomethingImportant();
+        }
+    }
+}
